9eda4166449a1bec20b50ad21c0d7de0fbecbe91
Mannat-Rana <mannat.rana120@gmail.com>
2021-11-08 20:06:17 -0700

    Added some debug messages

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
+    std::cout << "Starting main" << std::endl;
     std::cout << rt::Transforms::getRotationMatrix(rt::Axes::axis_z, 90) << std::endl;
+    std::cout << "Finished main" << std::endl;
     return 0;
 }
