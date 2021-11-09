74e2b7febd44202522e98b9db1e79766b7039831
Mannat-Rana <mannat.rana120@gmail.com>
2021-11-08 22:55:25 -0700

    Added forward kinematics functionality.

--- src/RoboticsToolbox.hpp
+++ src/RoboticsToolbox.hpp
@@ -3,6 +3,8 @@
 #include <eigen3/Eigen/Dense>
 #include <iostream>
 #include <math.h>
+#include <string>
+#include <vector>
 
 //TYPE ALIASES HERE
 using rotationMatrix_t = Eigen::Matrix3d;
@@ -13,6 +15,7 @@ using degreeAngle_t = double;
 
 namespace rt
 {
+    int getDOF(int M, int N, int J, const std::vector<int>& fi);
     enum Axes
     {
         axis_x,
@@ -20,6 +23,14 @@ namespace rt
         axis_z,
         axis_count,
     };
+    
+    enum LinkType
+    {
+        type_revolute,
+        type_prismatic,
+        type_count,
+    };
+
     namespace Constants
     {
         constexpr double pi = 3.14159265358979323846264;
@@ -70,4 +81,76 @@ namespace rt
          */
         rotationMatrix_t getRotationMatrix(Axes axis, double angle);
     }
+
+    struct DHParams 
+    {
+        double alpha;
+        double a;
+        double theta;
+        double d;
+    };
+    class Link
+    {
+        private:
+            DHParams m_params;
+            std::string m_name;
+        public:
+            Link();
+            Link(const DHParams& params);
+            Link(const std::string& name);
+            Link(const DHParams& params, const std::string& name);
+            void setAlpha(const double alpha);
+            void setA(const double a);
+            void setTheta(const double theta);
+            void setD(const double d);
+            void setName(const std::string& name);
+            double getAlpha() const;
+            double getA() const;
+            double getTheta() const;
+            double getD() const;
+            std::string getName() const;
+            virtual void printDHTable() const;
+            homogenousTransform_t getHomogenousTransform() const;
+    };
+    class Revolute : public Link
+    {
+        private:
+            rt::LinkType m_type;
+        public:
+            Revolute();
+            Revolute(const DHParams& params);
+            Revolute(const std::string& name);
+            Revolute(const DHParams& params, const std::string& name);
+            Revolute(const Link& link);
+            virtual void printDHTable() const;
+    };
+
+    class Prismatic : public Link
+    {
+        private:
+            rt::LinkType m_type;
+        public:
+            Prismatic();
+            Prismatic(const DHParams& params);
+            Prismatic(const std::string& name);
+            Prismatic(const DHParams& params, const std::string& name);
+            Prismatic(const Link& link);
+            virtual void printDHTable() const;
+    };
+    class Robot
+    {
+        private:
+            std::vector<Link> m_links;
+            std::string m_name;
+        public:
+            Robot();
+            Robot(const std::vector<Link>& links);
+            Robot(const std::string& name);
+            Robot(const std::vector<Link>& links, const std::string& name);
+            std::string getName() const;
+            void setName(const std::string& name);
+            void addLink(const Link& link);
+            homogenousTransform_t getForwardKinematics() const;
+            void printDHTable() const;
+    };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 int main()
 {
     std::cout << "Starting main" << std::endl;
-    std::cout << rt::Transforms::getRotationMatrix(rt::Axes::axis_z, 90) << std::endl;
-    std::cout << "Finished main" << std::endl;
+    rt::DHParams params {30, 1, 30, 1};
+    rt::Link link1 {params, "robot1"};
+    rt::Revolute rev1 {link1};
+    rt::Prismatic pri1 {link1};
+    std::vector<rt::Link> links {link1, rev1, pri1};
+    rt::Robot bot (links, "test robot");
+    bot.printDHTable();
     return 0;
 }
